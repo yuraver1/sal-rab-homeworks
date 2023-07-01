8464a40dda9ad4146514f551d1b434a59e1850c4
YuraVer <y.verzhbitskiy@gmail.com>
2023-07-01 21:15:55 +0300

    HomeWork tak4 v.1

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,12 +1,28 @@
 // Задание 4.1. Вывод карточек товаров
 
+
 function parseProducts(json) {
+  let data = JSON.parse(json);
+  let products = data.products;
+  return products;
     // Аргументом функции является JSON
 
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
+}
+
+
+function renderProductsCards(json)
+{
+    function clearProducts();
+    let products = parseProducts(json);
+    let length = products.length;
+    for (let i = 0; i < length; i += 1)
+    {
+        addProduct(products[i]);
+    } 
 }
 
 // Напишите функцию renderProductsCards(json)
