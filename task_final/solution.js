7a5ca4e1427289cb409e20bcde3368ce417b3c62
YuraVer <y.verzhbitskiy@gmail.com>
2023-07-04 21:24:46 +0300

    homeWorkFinalTaskv1

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,20 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {goods: [], order: {address, sum}, client: {name, phone}};
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i ++) {
+        data.goods.push({title:goods[i].title, count:goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж," + " кв. " + address.flat;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
+    data.client = `${name}` + " " + `${phone}`;
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data:data});
 
     return jsonData;
 }
